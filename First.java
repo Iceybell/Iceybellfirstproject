6d2b9e05c94cf2445e94d80a63ffab2145e97805
Iceybell <50933928+Iceybell@users.noreply.github.com>
2019-05-23 09:39:53 +0800

    修改内容
    修改为：会员卡号和姓名录入

--- First.java
+++ First.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class First{
 	public static void main(String[] args) {
-		System.out.println("hello！！！");
+		Scanner input=new Scanner(System.in);
+		//输入姓名  会员卡号四位，分别输出各位数上的数字，并求和。如果等于随机出现的数字为中奖
+		
+		System.out.println("请输入用户姓名：");
+		String name=input.next();
+		System.out.println("四位数的会员卡号：");
+		int num=input.nextInt();
+		System.out.println("四位数的会员卡号："+num);
+
 	}
 }
